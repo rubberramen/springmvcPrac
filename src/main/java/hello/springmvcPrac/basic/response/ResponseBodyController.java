8b11d1dd80df98127fe6879da2bf5f69afd65838
Chanhyun Wang <rubberramen@naver.com>
2023-10-04 10:27:05 +0900

    section6 : ~HTTP 응답 - HTTP API, 메시지 바디에 직접 입력

---
+++ src/main/java/hello/springmvcPrac/basic/response/ResponseBodyController.java
@@ -0,0 +1,33 @@
+package hello.springmvcPrac.basic.response;
+
+import hello.springmvcPrac.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+//        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
+    }
+}
